e4f7ccece2bcffb583482aaa377ae8a86e7fa90a
ahandey <ahanaditya.dey@gmail.com>
2025-10-23 01:07:41 -0500

    Fix Event Set in "index.js"

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
+// For every file in the events folder
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 for (const file of eventFiles) {
+	// For every event defined by the file
 	const filePath = path.join(eventsPath, file);
 	const eventSet = require(filePath);
-	for (const event of eventSet) {
+	for (const event of eventSet.event) {
+		// If the event is to happen once, do it, otherwise do it every time
 		if (event.once) {
 			client.once(event.name, (...args) => event.execute(...args));
 		} else {
